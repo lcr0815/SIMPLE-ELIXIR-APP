676e3e5a1114f55eca48960e60daa9875f48f1c9
LC <luis.carlos.dcu@gmail.com>
2023-01-19 15:12:09 -0500

    add project files

---
+++ estee/lib/lc_estee_auder_code_assessment.ex
@@ -0,0 +1,18 @@
+defmodule LcEsteeAuderCodeAssessment do
+  @moduledoc """
+  Documentation for `LcEsteeAuderCodeAssessment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LcEsteeAuderCodeAssessment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
